2c6c7fde61e3a1fff893d6dd60e550c89c6aaae2
Konstantin S <havendv@gmail.com>
2023-09-28 07:42:53 +0400

    chore: Removed unused files.

--- example/test.Server/GlobalUsings.cs
+++
@@ -1,3 +0,0 @@
-global using Microsoft.AspNetCore.Mvc;
-global using test.DataContracts;
-global using test.Server.Apis;
--- example/test/MainPage.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace test;
-
-public sealed partial class MainPage : Page
-{
-	public MainPage()
-	{
-		this.InitializeComponent();
-	}
-}
