30d4b29f5eebbb3811a8e28aaa70c2040c36ca1a
Konstantin S <havendv@gmail.com>
2023-10-07 14:20:04 +0400

    fix: Released 0.17.2.

--- scripts/workload-install.ps1
+++ scripts/workload-install.ps1
@@ -49,7 +49,7 @@ function Ensure-Directory([string]$TestDir) {
 }
 
 function Get-LatestVersion([string]$Id) {
-    return "0.17.1"
+    return "0.17.2"
     
     $attempts=3
     $sleepInSeconds=3
