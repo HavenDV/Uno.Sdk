82ee53f7871a2d79ff054b419528f4e3ee17fb74
Konstantin S <havendv@gmail.com>
2023-10-07 05:21:23 +0400

    feat: Added UnoProgram.cs.

--- scripts/workload-install.ps1
+++ scripts/workload-install.ps1
@@ -49,7 +49,7 @@ function Ensure-Directory([string]$TestDir) {
 }
 
 function Get-LatestVersion([string]$Id) {
-    return "0.16.0"
+    return "0.17.0"
     
     $attempts=3
     $sleepInSeconds=3
