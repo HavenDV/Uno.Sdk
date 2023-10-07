df43a89bbef8ca2eaf54acd8ffc8418ad9a22670
Konstantin S <havendv@gmail.com>
2023-10-07 04:59:28 +0400

    feat: Moved Mobile specific folders to Platforms subfolder.

--- scripts/workload-install.ps1
+++ scripts/workload-install.ps1
@@ -49,7 +49,7 @@ function Ensure-Directory([string]$TestDir) {
 }
 
 function Get-LatestVersion([string]$Id) {
-    return "0.15.0"
+    return "0.16.0"
     
     $attempts=3
     $sleepInSeconds=3
