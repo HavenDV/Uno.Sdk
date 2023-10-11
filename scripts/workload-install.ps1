377055d3eded29909650a261df6567eca12ba4a3
Konstantin S <havendv@gmail.com>
2023-10-11 11:47:42 +0400

    feat: Added ability to disable implicit packages.

--- scripts/workload-install.ps1
+++ scripts/workload-install.ps1
@@ -49,7 +49,7 @@ function Ensure-Directory([string]$TestDir) {
 }
 
 function Get-LatestVersion([string]$Id) {
-    return "0.17.2"
+    return "0.18.0"
     
     $attempts=3
     $sleepInSeconds=3
