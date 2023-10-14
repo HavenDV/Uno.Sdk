aa351bbb40076438b2342f5388d61e7e08d68bd8
Konstantin S <havendv@gmail.com>
2023-10-14 07:15:32 +0400

    apps: Added Uno.Maui.Example.

---
+++ src/apps/Uno.Maui.Example/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Uno.Maui.Example;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ src/apps/Uno.Maui.Example/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Uno.Maui.Example;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
